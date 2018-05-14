3c183d3712dc1b30b61281697c0281666de28fd3
mbrenesn <mbrenes@ictp.it>
2018-05-14 19:04:12 +0100

    Minor change to SVD

--- MPyS/Utils/GeneralUtils.py
+++ MPyS/Utils/GeneralUtils.py
@@ -9,7 +9,7 @@ def svd(t):
         U, S, V = np.linalg.svd(t, full_matrices = False)
     except np.linalg.linalg.LinAlgError:
         print "# Convergence problems occured in SVD!"
-        U, S, V = np.linalg.svd(t + (1.0E-15 * np.random.randn(*t.shape)), full_matrices = False)
+        U, S, V = np.linalg.svd(t + (1.123E-11 * np.random.randn(*t.shape)), full_matrices = False)
 
     return U, S, V
 
